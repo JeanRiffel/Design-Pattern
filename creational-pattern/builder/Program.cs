5632986f23d3b459a24e6febb3922f2eaf0ceea7
JeanRiffel <jeanriffel1@gmail.com>
2020-07-04 10:29:55 -0300

    files updated

---
+++ creational-pattern/builder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ordinary non-fluent builder
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello");
+            builder.AddChild("li", "world");
+            Console.WriteLine(builder.ToString());
+
+            //fluent builder
+            builder.Clear();
+            builder.AddChildFluent("li", "hello").AddChildFluent("li", "world");            
+            Console.WriteLine(builder);
+
+
+
+        }
+    }
+}
